53fff0af7a372a069a63a99c7aaa71fdf311e458
Элиза Арсаева <earsaeva@diasoft.ru>
2023-05-23 02:15:33 +0400

    init timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [currentSeconds, setCurrentSeconds] = useState(0);
+
+  useEffect(() => {
+    if (currentSeconds <= 0) return;
+    const timeout = setTimeout(() => {
+      setCurrentSeconds((currentSeconds) => currentSeconds - 1);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [currentSeconds]);
+
+  const handleChange = (e) => {
+    setValue(e.target.value.replace(/[^0-9]/g, ''));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        onChange={handleChange}
+        value={value}
+        placeholder='Seconds'
+        type='text'
+      />
+
+      <button onClick={() => setCurrentSeconds(Number(value))}>Start</button>
+
+      <br />
+      <br />
+
+      <span>
+        {Math.floor((currentSeconds / 60 / 60) % 60)}:{' '}
+        {Math.floor((currentSeconds / 60) % 60)} :{' '}
+        {Math.floor(currentSeconds % 60)}
+      </span>
+      <br />
+
+      <span>hh:mm:ss</span>
     </div>
   );
 }
